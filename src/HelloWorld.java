a65a96d94f39803bfef1052c51c41ba1fc16eddd
joseph Joice <josepjoice@gmail.com>
2015-04-13 17:09:18 +0530

    Finding sum

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,21 @@
+import sun.rmi.runtime.Log;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 /**
  * Created by joseph on 13/4/15.
  */
 public class HelloWorld {
-    public static void main(String args[])
-    {
-        System.out.println("HelloWorld!");
+    public static void main(String args[]) {
+        InputStreamReader in = new InputStreamReader(System.in);
+        BufferedReader read = new BufferedReader(in);
+        try {
+            int num1 = Integer.parseInt(read.readLine());
+            int num2 = Integer.parseInt(read.readLine());
+            System.out.println("The sum is " + (num1 + num2));
+        } catch (Exception e) {
+            System.out.println("Some error occured");
+        }
     }
 }
